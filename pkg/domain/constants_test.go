17ce7d8bc0aaebc424046b923a9cdfe68a9cbf11
Ricardo Rivera <25689973+silkrad@users.noreply.github.com>
2026-05-08 11:39:30 -0500

    test(domain): add constants golden test

---
+++ pkg/domain/constants_test.go
@@ -0,0 +1,20 @@
+package domain_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/abilisoft/usbip-go/pkg/domain"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConstants_KnownValues(t *testing.T) {
+	require.Equal(t, uint16(3240), domain.DefaultPort)
+	require.Equal(t, "0.0.0.0:3240", domain.DefaultEndpoint)
+	require.Equal(t, uint16(0x0111), domain.ProtocolVersion)
+	require.Equal(t, 32, domain.BusIDSize)
+	require.Equal(t, 256, domain.SysPathSize)
+	require.Equal(t, 10*time.Second, domain.DefaultDialTimeout)
+	require.Equal(t, 5*time.Second, domain.DefaultHandshakeTimeout)
+	require.Equal(t, 30*time.Second, domain.DefaultShutdownTimeout)
+}
